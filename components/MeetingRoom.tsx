99782a20d53ed37c3426dc117f3031e982f41d12
BakarTheWise <abubakarmuaz60@gmail.com>
2024-11-29 16:21:24 +0300

    fo sho da last one

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -37,12 +37,12 @@ const MeetingRoom = () => {
   const isPersonalRoom = !!searchParams.get('personal');
   const router = useRouter();
 
-  const [hasNewMessage, setHasNewMessage] = useState(false); // Tracks if there's a new message
+  const [hasNewMessage, setHasNewMessage] = useState(false);
   const [layout, setLayout] = useState<CallLayoutType>('speaker-left');
   const [activeTab, setActiveTab] = useState<'participants' | 'messages'>(
     'participants'
   );
-  const [sidebarOpen, setSidebarOpen] = useState(true);
+  const [sidebarOpen, setSidebarOpen] = useState(false);
   const { useCallCallingState } = useCallStateHooks();
 
   const callingState = useCallCallingState();
@@ -50,21 +50,17 @@ const MeetingRoom = () => {
   useEffect(() => {
     if (!client) return;
 
-    // Show the red circle when a new message is received
     const handleNotification = () => {
       setHasNewMessage(true);
     };
 
-    // Hide the red circle when messages are read
     const handleMarkRead = () => {
       setHasNewMessage(false);
     };
 
-    // Add event listeners
     client.on('notification.message_new', handleNotification);
     client.on('notification.mark_read', handleMarkRead);
 
-    // Cleanup on unmount
     return () => {
       client.off('notification.message_new', handleNotification);
       client.off('notification.mark_read', handleMarkRead);
@@ -85,18 +81,19 @@ const MeetingRoom = () => {
   };
 
   return (
-    <section className="relative h-screen w-full text-white overflow-hidden">
-      <div className="relative flex h-full w-full justify-center items-center">
+    <section className="relative h-screen w-full text-white !overflow-hidden">
+      <div className="relative flex flex-col h-full w-full justify-center md:flex-row">
         {/* Main Content Area */}
         <div
-          className={`h-full flex flex-col items-center justify-between flex-1 max-w-[1000px] transition-all duration-300 ease-in-out ${
-            sidebarOpen ? 'md:pr-[15rem]' : 'md:pr-20'
+          className={`pt-2 h-full flex flex-col items-center justify-between flex-1 w-full max-w-[1000px] transition-all duration-300 ease-in-out ${
+            sidebarOpen ? 'md:mr-[20rem]' : 'md:mr-20'
           }`}
         >
           <div className="flex flex-1 items-center w-full">
             <CallLayout />
           </div>
-          <div className="flex w-full items-center justify-center gap-5">
+          {/* Call Controls */}
+          <div className="flex w-full items-center justify-center gap-3 py-2 md:py-0 md:gap-5 bg-black md:bg-transparent">
             <CallControls onLeave={() => router.push(`/`)} />
             <button
               className="rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]"
@@ -105,14 +102,14 @@ const MeetingRoom = () => {
                 setActiveTab('participants');
               }}
             >
-              <User2></User2>
+              <User2 />
             </button>
             <button
               className="relative rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]"
               onClick={() => {
                 setSidebarOpen(true);
                 setActiveTab('messages');
-                setHasNewMessage(false); // Mark messages as read when opening
+                setHasNewMessage(false);
               }}
             >
               <MessageCircle className="cursor-pointer text-gray-500" size={24} />
@@ -121,24 +118,20 @@ const MeetingRoom = () => {
               )}
             </button>
             <DropdownMenu>
-              <div className="flex items-center">
-                <DropdownMenuTrigger className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]">
-                  <LayoutList size={20} className="text-white" />
-                </DropdownMenuTrigger>
-              </div>
+              <DropdownMenuTrigger className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]">
+                <LayoutList size={20} className="text-white" />
+              </DropdownMenuTrigger>
               <DropdownMenuContent className="border-dark-1 bg-dark-1 text-white">
                 {['Grid', 'Speaker-Left', 'Speaker-Right'].map(
                   (item, index) => (
-                    <div key={index}>
-                      <DropdownMenuItem
-                        onClick={() =>
-                          setLayout(item.toLowerCase() as CallLayoutType)
-                        }
-                      >
-                        {item}
-                      </DropdownMenuItem>
-                      <DropdownMenuSeparator className="border-dark-1" />
-                    </div>
+                    <DropdownMenuItem
+                      key={index}
+                      onClick={() =>
+                        setLayout(item.toLowerCase() as CallLayoutType)
+                      }
+                    >
+                      {item}
+                    </DropdownMenuItem>
                   )
                 )}
               </DropdownMenuContent>
@@ -150,79 +143,65 @@ const MeetingRoom = () => {
 
         {/* Sidebar */}
         <div
-          className={cn(
-            'bordering sidetrans fixed right-2 h-[94%]  shadow-md flex flex-col transition-all duration-400 ease-in-out',
-            sidebarOpen ? 'translate-x-0' : 'max-w-0 !border-none'
-          )}
-          style={{ width: '25%' }}
-        >
-          {/* Close Button */}
-          <div className="flex justify-between items-center px-4 py-2">
-            <h3 className="text-lg font-medium text-white">
-              {activeTab === 'participants' ? 'Participants' : 'Chat'}
-            </h3>
-            <button
-              className="rounded-md p-1 hover:bg-gray-700"
-              onClick={() => setSidebarOpen(false)}
-            >
-              <X size={20} className="text-white" />
-            </button>
-          </div>
+  className={cn(
+    'fixed top-0 right-0 h-full w-full md:w-[25%]  shadow-md flex flex-col transition-transform duration-300 ease-in-out',
+    sidebarOpen ? 'translate-x-0' : 'translate-x-full'
+  )}
+>
+  <div className="flex justify-between items-center px-4 py-2">
+    <h3 className="text-lg font-medium text-white">
+      {activeTab === 'participants' ? 'Participants' : 'Chat'}
+    </h3>
+    <button
+      className="rounded-md p-1 hover:bg-gray-700"
+      onClick={() => setSidebarOpen(false)}
+    >
+      <X size={20} className="text-white" />
+    </button>
+  </div>
 
-          {/* Tab Navigation */}
-          <div className="flex justify-between px-4 py-2">
-            <button
-              className={cn(
-                'w-1/2 px-4 py-2 text-center',
-                activeTab === 'participants'
-                  ? 'bg-gray-700 text-white'
-                  : 'bg-gray-800 text-gray-400 hover:bg-gray-700'
-              )}
-              onClick={() => setActiveTab('participants')}
-            >
-              Participants
-            </button>
-            <button
-              className={cn(
-                'w-1/2 px-4 py-2 text-center',
-                activeTab === 'messages'
-                  ? 'bg-gray-700 text-white'
-                  : 'bg-gray-800 text-gray-400 hover:bg-gray-700'
-              )}
-              onClick={() => setActiveTab('messages')}
-            >
-              Messages
-            </button>
-          </div>
+  <div className="flex justify-between px-4 py-2">
+    <button
+      className={cn(
+        'w-1/2 px-4 py-2 text-center',
+        activeTab === 'participants'
+          ? 'bg-gray-700 text-white'
+          : 'bg-gray-800 text-gray-400 hover:bg-gray-700'
+      )}
+      onClick={() => setActiveTab('participants')}
+    >
+      Participants
+    </button>
+    <button
+      className={cn(
+        'w-1/2 px-4 py-2 text-center',
+        activeTab === 'messages'
+          ? 'bg-gray-700 text-white'
+          : 'bg-gray-800 text-gray-400 hover:bg-gray-700'
+      )}
+      onClick={() => setActiveTab('messages')}
+    >
+      Messages
+    </button>
+  </div>
 
-          {/* Tab Content */}
-          <div className="trans-chat flex-1 overflow-hidden">
-            {activeTab === 'participants' && (
-              <div className="h-full overflow-y-auto">
-                <CallParticipantsList
-                  onClose={() => setActiveTab('messages')}
-                />
-              </div>
-            )}
-            {activeTab === 'messages' && (
-              <ChannelProvider callId={id}>
-                <Window>
-                  <div className="flex flex-col h-full">
-                    {/* Chat Header */}
-                    {/* Message List */}
-                    <div className="h-[90%] trans">
-                      <MessageList />
-                    </div>
-                    {/* Chat Input */}
-                    <div className="!px-6 flex-shrink-0">
-                      <MessageInput />
-                    </div>
-                  </div>
-                </Window>
-              </ChannelProvider>
-            )}
-          </div>
-        </div>
+  <div className="flex-1 overflow-y-auto">
+    {activeTab === 'participants' && (
+      <div className="h-full overflow-y-auto">
+        <CallParticipantsList onClose={() => setActiveTab('messages')} />
+      </div>
+    )}
+    {activeTab === 'messages' && (
+      <ChannelProvider callId={id}>
+        <Window>
+          <MessageList />
+          <MessageInput />
+        </Window>
+      </ChannelProvider>
+    )}
+  </div>
+</div>
+
       </div>
     </section>
   );
